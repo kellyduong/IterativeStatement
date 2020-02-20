7729090c68a74132b79641ddfbd8b1586bacf750
Kelly Duong <kellyduong@usf.edu>
2020-02-19 20:41:55 -0500

    Added for loop for even numbers

--- Program.cs
+++ Program.cs
@@ -9,11 +9,15 @@ namespace IterativeStatement
             Console.WriteLine("Enter an integer value between 2 and 10");
 
             int n = Convert.ToInt32(Console.ReadLine());
-
-            if (n % 2 == 1)
+            if (n%2 == 1)
             {
-                for (int i = 0; i < n - 1; n++)
+                for (int i = 0; i < n - 1; i++)
                     Console.WriteLine("You entered an odd number");
+            }
+            else
+            {
+                for (int i = 0; i < n + 1; i++)
+                    Console.WriteLine("You entered an even number");
             }
         }
     }
