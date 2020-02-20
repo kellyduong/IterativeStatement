71e168551ae005656aa149b365cdd6665222eaff
Kelly Duong <kellyduong@usf.edu>
2020-02-19 20:31:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer value between 2 and 10");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 1)
+            {
+                for (int i = 0; i < n - 1; n++)
+                    Console.WriteLine("You entered an odd number");
+            }
+        }
+    }
+}
