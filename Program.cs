2afc9cbde780b353e89a12d748445dc0c411c8be
Kelly Duong <kellyduong@usf.edu>
2020-02-19 20:46:25 -0500

    if else statement

--- Program.cs
+++ Program.cs
@@ -9,15 +9,15 @@ namespace IterativeStatement
             Console.WriteLine("Enter an integer value between 2 and 10");
 
             int n = Convert.ToInt32(Console.ReadLine());
-            if (n%2 == 1)
-            {
-                for (int i = 0; i < n - 1; i++)
-                    Console.WriteLine("You entered an odd number");
-            }
-            else
+            if (n % 2 == 0)
             {
                 for (int i = 0; i < n + 1; i++)
                     Console.WriteLine("You entered an even number");
+            }
+            else
+            {
+                for (int i = 0; i < n - 1; i++)
+                    Console.WriteLine("You entered an odd number");
             }
         }
     }
